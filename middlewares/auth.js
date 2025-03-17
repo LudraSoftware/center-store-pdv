22839461703ed55f9d8b0062f314ab53c8ff5fab
Luan Andrade <luanandradeti10@gmail.com>
2025-03-16 21:00:58 -0300

    feat: modify authentication middleware to bypass admin check for testing purposes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,23 @@
 module.exports = {
-    ensureAuthenticated: (req, res, next) => {
-      if (req.isAuthenticated()) {
-        return next();
-      }
-      req.flash('error', 'Você precisa estar logado para acessar esta página.');
-      res.redirect('/login');
-    },
-    
-    ensureAdmin: (req, res, next) => {
-        if (req.isAuthenticated() && req.user.type === 'admin') {
-          return next();
-        }
-        req.flash('error', 'Acesso negado! Apenas administradores podem acessar esta página.');
-        res.redirect('/dashboard');
-      }
-  };
-  
+  ensureAuthenticated: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash("error", "Você precisa estar logado para acessar esta página.");
+    res.redirect("/login");
+  },
+
+  ensureAdmin: (req, res, next) => {
+    // if (req.isAuthenticated() && req.user.type === 'admin') {
+    //   return next();
+    // }
+    if (true) {
+      return next();
+    }
+    req.flash(
+      "error",
+      "Acesso negado! Apenas administradores podem acessar esta página."
+    );
+    res.redirect("/dashboard");
+  },
+};
